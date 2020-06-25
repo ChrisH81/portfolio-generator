5443cdc33d5130fe86e6b9812661b0aeac42df74
Chris H <64100619+ChrisH81@users.noreply.github.com>
2020-06-24 20:30:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,38 @@ var sum = 5 + 3;
 console.log(message);
 console.log(sum);
 
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+  console.log(profileDataArr);
+};
+
+printProfileData(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+  console.log(profileDataArr);
+};
+
+const addNums = (numOne, numTwo) => numOne + numTwo;
+
+const sum = addNums(5, 3); // sum would be 8
+
+const addNums = (numOne, numTwo) => {
+  console.log(numOne, numTwo);
+  return numOne + numTwo;
+};
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+};
+
+console.log('================');
+
+profileDataArr.forEach(profileItem => console.log(profileItem));
